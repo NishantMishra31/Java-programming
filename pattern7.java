5ec3cd4a0d6dacf369d839c79ff3be335f12c1bd
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-03 17:49:57 +0530

    Added Files and code changes

--- pattern7.java
+++ pattern7.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class pattern6 {
+public class pattern7 {
     public static void main(String args[]){
 
-        // number pyramid 
+        // inverted half pyramid of numbers
         
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
-        for(int i = 1; i <= n; i++){
+        for(int i = n; i >= 1; i--){
             for(int j = 1; j <= i; j++){
                 System.out.print(j + " ");
             }
