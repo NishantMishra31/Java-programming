233878109d71c495ae38e8b5cc43b811e04de823
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-04 09:29:16 +0530

    Added Files and code changes

--- pattern11.java
+++ pattern11.java
@@ -2,11 +2,21 @@ import java.util.Scanner;
 
 public class pattern11 {
     public static void main(String[] args) {
+
+        // solid rhombus
+        
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
         for(int i = 1; i <=n; i++){
-
+            int spaces = n - i;
+            for(int j = 1; j <= spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
