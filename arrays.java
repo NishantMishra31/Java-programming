66dbdfcf94967c038249cfdb501ca6df977fdfdc
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-17 17:40:02 +0530

    Added Files and code changes

---
+++ arrays.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class arrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] num = new int[n];
+        for(int i = 0; i < n; i++){
+            num[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++){
+            System.out.println(num[i]);
+        }
+        int x = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            if(num[i] == x) System.out.println("index: " + i);
+        }
+        sc.close();
+    }
+}
