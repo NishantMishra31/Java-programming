bdc653ff6481dc16c81ffde6bcc79536305d71b0
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-18 23:50:43 +0530

    Added Files and code changes

---
+++ strings.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class strings {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        String firstName = sc.next();
+        String lastName = sc.next();
+        System.out.println("Your Name is: " + firstName + " " + lastName);
+        sc.nextLine();  // to read buffer next line
+        String sentence = sc.nextLine();
+        System.out.println(sentence);
+        sc.close();
+    }
+}
