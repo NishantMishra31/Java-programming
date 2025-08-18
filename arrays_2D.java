3a5b7a63195369f25f822ab87f6c5b916c1cca2e
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-18 23:36:44 +0530

    Added Files and code changes

---
+++ arrays_2D.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class arrays_2D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] matrix = new int[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int x = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == x){
+                    System.out.print(i + ", " + j);
+                }
+            }
+        }
+        sc.close();
+    }
+}
