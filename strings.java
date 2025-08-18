acddcc278c4e2164f7034af5d2410f55312718e5
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-19 00:35:59 +0530

    Added Files and code changes

--- strings.java
+++ strings.java
@@ -7,10 +7,31 @@ public class strings {
         Scanner sc  = new Scanner(System.in);
         String firstName = sc.next();
         String lastName = sc.next();
-        System.out.println("Your Name is: " + firstName + " " + lastName);
+        String fullName = firstName + " " + lastName; // concatenation
+        System.out.println(fullName);
         sc.nextLine();  // to read buffer next line
         String sentence = sc.nextLine();
         System.out.println(sentence);
+
+        // length
+        System.out.println(fullName.length());
+
+        // charAt
+        for(int i = 0; i < fullName.length(); i++){
+            System.out.print(fullName.charAt(i));
+        }
+        System.out.println();
+
+        // compareTo
+        if(firstName.compareTo(lastName) == 0){
+            System.out.println("same entry both places");
+        }
+        else System.out.println("good entries");
+
+        // substring
+        String first3letters = fullName.substring(0, 3);
+        System.out.println(first3letters);
+        
         sc.close();
     }
 }
