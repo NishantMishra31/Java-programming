9858e72ccad3fc3e30ae2aa003be47d71adc5af4
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-19 03:38:05 +0530

    Added Files and code changes

---
+++ reverse_string.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class reverse_string {
+
+    public static void swap(StringBuilder str, int i, int j) {
+        char temp = str.charAt(i);
+        str.setCharAt(i, str.charAt(j));
+        str.setCharAt(j, temp);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder str = new StringBuilder(sc.nextLine());
+        System.out.println(str);
+        int l = 0, r = str.length() - 1;
+        while(l < r){
+            swap(str, l, r);
+            l++;
+            r--;
+        }
+        System.out.println(str);
+        sc.close();
+    }
+}
