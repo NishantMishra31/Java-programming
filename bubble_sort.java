d1aeaee5034ac2c2d0a0e178b3015e140a05e46e
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-20 03:52:34 +0530

    Added Files and code changes

--- bubble_sort.java
+++ bubble_sort.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class bubble_sort {
 
     // this takes O(N^2) TC 
-    
+
     public static void bubble(int[] arr) {
         int n = arr.length;
-        for(int i = n - 1; i >= 1; i--){
-            for(int j = 0; j < i; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n - i - 1; j++){
                 if(arr[j] > arr[j + 1]){
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
