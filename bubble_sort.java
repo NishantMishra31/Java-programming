e4d2d01cc619039f5f5d2511ce90963c1a2fd80e
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-20 03:43:47 +0530

    Added Files and code changes

---
+++ bubble_sort.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class bubble_sort {
+
+    public static void bubble(int[] arr) {
+        int n = arr.length;
+        for(int i = n - 1; i >= 1; i--){
+            for(int j = 0; j < i; j++){
+                if(arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        // call
+        System.out.println();
+        bubble(arr);
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
