7d2073ec23e9281cf9cdf16ed89f53ed625fdf75
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-20 04:00:55 +0530

    Added Files and code changes

--- selection_sort.java
+++ selection_sort.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
-public class bubble_sort {
+public class selection_sort {
 
     // this takes O(N^2) TC 
 
-    public static void bubble(int[] arr) {
+    public static void selection(int[] arr) {
         int n = arr.length;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n - i - 1; j++){
-                if(arr[j] > arr[j + 1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+        for(int i = 0; i < n - 1; i++){
+            int mini = i;
+            for(int j = i + 1; j < n; j++){
+                if(arr[j] < arr[mini]) mini = j;
             }
+            int temp = arr[mini];
+            arr[mini] = arr[i];
+            arr[i] = temp;
         }
     }
     public static void main(String[] args) {
@@ -28,7 +28,7 @@ public class bubble_sort {
         }
         // call
         System.out.println();
-        bubble(arr);
+        selection(arr);
         for(int i = 0; i < n; i++){
             System.out.print(arr[i] + " ");
         }
