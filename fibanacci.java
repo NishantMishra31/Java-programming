e5c34ec7019117b768bcd656a2da55db51d9a6bb
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-22 18:54:34 +0530

    Added Files and code changes

---
+++ fibanacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class fibanacci {
+
+    public static void printFib(int a, int b, int n){
+        if(n == 0) return;
+        int c = a + b;
+        System.out.print(c + ", ");
+        printFib(b, c, n - 1);
+    }
+    public static void main(String[] args) {
+        int a = 0, b = 1;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(a + ", " + b + ", ");
+        printFib(a, b, n - 2);
+        sc.close();
+    }
+}
