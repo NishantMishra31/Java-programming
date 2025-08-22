eb06f12795ecd6663fa1dddb325abaaa42aeff91
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-22 18:24:13 +0530

    Added Files and code changes

---
+++ fact_recursion.java
@@ -0,0 +1,11 @@
+public class fact_recursion {
+
+    public static int facto(int n) {
+        if (n == 0 || n == 1) return 1;
+        return n * facto(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(facto(5));
+    }
+}
