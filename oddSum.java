cb7d4ce2a69037caa6702b2a064a77ef79245b5f
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-23 23:11:52 +0530

    Added Files and code changes

--- oddSum.java
+++ oddSum.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class oddSum {
     public static int printSum(int n){
+
+        // prints sum of first n odd numbers
         int sum = 0;
         while(n > 0){
             if(n % 2 == 1) sum += n;
