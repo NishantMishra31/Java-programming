aa24f71465e2777b5c9c4d127b275004f64b77ed
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-23 23:09:22 +0530

    Added Files and code changes

--- fact_recursion.java
+++ fact_recursion.java
@@ -1,5 +1,6 @@
 public class fact_recursion {
-
+    
+    // calculate factorial of a number using recursion
     public static int facto(int n) {
         if (n == 0 || n == 1) return 1;
         return n * facto(n - 1);
