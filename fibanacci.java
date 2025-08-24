63a7f1df2d7cace17dfa4a648c05feca7bb24298
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-24 22:56:41 +0530

    Added Files and code changes

--- fibanacci.java
+++ fibanacci.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class fibanacci {
     
-    // to print the fibonacci sequence till n terms
+    // to print the fibonacci sequence till n terms of input
     public static void printFib(int a, int b, int n){
         if(n == 0) return;
         int c = a + b;
