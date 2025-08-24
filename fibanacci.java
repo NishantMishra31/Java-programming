55bd2920a2b59e12f159b2f9b7fadf832d689de3
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-24 23:00:58 +0530

    Added Files and code changes

--- fibanacci.java
+++ fibanacci.java
@@ -13,8 +13,11 @@ public class fibanacci {
         int a = 0, b = 1;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.print(a + ", " + b + ", "); // prints first two terms manually
-        printFib(a, b, n - 2);
+        if(n == 1) System.out.println(a); 
+        else{
+            System.out.print(a + ", " + b + ", "); // prints first two terms manually
+            printFib(a, b, n - 2);
+        }
         sc.close();
     }
 }
