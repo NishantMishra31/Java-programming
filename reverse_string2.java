e9ea0285c22bd554766ec2d98ab147212ced8730
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-28 05:21:42 +0530

    Added Files and code changes

--- reverse_string2.java
+++ reverse_string2.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class reverse_string2 {
 
+    // reverse a string using recursion
     public static void reverse(String str, int idx) {
-        if(idx == 0){
+        if(idx == 0){ // base case
             System.out.print(str.charAt(idx));
             return;
         }
         System.out.print(str.charAt(idx));
-        reverse(str, idx - 1);
+        reverse(str, idx - 1); // recursive calls with decreasing index
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
