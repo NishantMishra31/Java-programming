d12cb35984f069af3cb5c51b4964d84f350a9beb
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-27 23:39:05 +0530

    Added Files and code changes

--- power.java
+++ power.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class power {
 
     public static int calcpower(int x, int n){
-        if(n == 0) return 1;
-        int pow = x * calcpower(x, n - 1);
+        if(n == 0) return 1; // base case
+        int pow = x * calcpower(x, n - 1); // recursive function
         return pow;
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int n = sc.nextInt();
-        System.out.println(calcpower(x, n));
+        Scanner sc = new Scanner(System.in); // input class
+        int x = sc.nextInt(); //input number
+        int n = sc.nextInt(); // input power/exponent
+        System.out.println(calcpower(x, n)); // call
         sc.close();
     }
 }
