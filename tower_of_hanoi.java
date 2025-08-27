857066c8b7660fae74c59bdb7195aa4602503996
Nishant Mishra <nishantmishra3108@gmail.com>
2025-08-28 05:12:13 +0530

    Added Files and code changes

--- tower_of_hanoi.java
+++ tower_of_hanoi.java
@@ -3,18 +3,18 @@ import java.util.Scanner;
 public class tower_of_hanoi {
 
     public static void tower(int n, String src, String helper, String dest) {
-        if(n == 1){
+        if(n == 1){ // base case
             System.out.println("transfer disk " + n + " from " + src + " to " + dest);
             return;
         }
-        tower(n - 1, src, dest, helper);
+        tower(n - 1, src, dest, helper); // passing different values for src, helper and dest towers
         System.out.println("transfer disk " + n + " from " + src + " to " + dest);
-        tower(n - 1, helper, src, dest);
+        tower(n - 1, helper, src, dest); // passing different values for src, helper and dest towers
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        tower(n, "A", "B", "C");
+        int n = sc.nextInt(); // input number of disks
+        tower(n, "A", "B", "C"); // call with tower names
         sc.close();
     }
 }
